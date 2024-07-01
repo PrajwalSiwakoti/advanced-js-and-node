7231189780f22c3da5cbd0efd290127668054357
Prajwal Siwakoti <prajwal.sw@gmail.com>
2024-07-01 10:12:33 +0545

    handling request data in core way

--- NetNinja/server.js
+++ NetNinja/server.js
@@ -8,6 +8,45 @@ const server =  http.createServer((req, res)=>{
     //basic routing
     let viewsPath = './views/';
 
+    //this is from oreilly
+    //we can also do like this, where we handle incoming request data in chunk
+    //bts how express works
+
+    if(req.url === '/message' && req.method === 'GET'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action = "/message" method="POST"><input type="text" name="message"/><button type="submit">Submit</button></form></body>');
+        res.write('</html>');
+        res.end(); //if return not mentiond console will print...res.end() will not terminate the execution
+        // res.end() is JUST a function call like any other function call. It does not affect the Javascript flow of control in your function at all. It terminates the http request, but not the function you're in. If there are lines of code after it in the function, they will execute. If you want to terminate the execution of the request handler, then use a return after the res.end() or structure an if/else logic so it only executes one thing and then is done.
+        return;
+    }
+
+    if(req.url === '/message' && req.method === 'POST'){
+
+        const body = [];
+        //we catch request body by registering event listener
+        //data event will be fired whenever a new chunk is ready to be read
+        req.on('data', (chunk)=>{
+            body.push(chunk);
+        });
+
+        //it will be fired once it is done parsing incoming request 
+        req.on('end', ()=>{
+            const parsedBody = Buffer.concat(body).toString(); //only works for text request body, we should do different for file
+            //console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('./message.txt',message);
+        });
+        
+        res.end();
+        return;
+    
+    }
+
+    //console.log('ljlhere');
+   // return;
+
     switch(req.url){
         case '/':
             viewsPath += 'index.html';
@@ -21,13 +60,17 @@ const server =  http.createServer((req, res)=>{
             //redirect
             res.statusCode = 301;
             res.setHeader('Location', '/about');
-            res.end
+            res.end();
+            return;
             break;
         default:
             viewsPath += '404.html';
             res.statusCode = 404;
             break;
     }
+
+  // console.log(viewsPath);
+    //return;
    
     fs.readFile(viewsPath, (err, data) => {
         //res.write(data);
